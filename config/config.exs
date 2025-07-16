8a8f70e843c87f6f3e049822dfb68cb7d572a13b
Pat Maddox <pat@evirts.com>
2025-07-16 13:50:20 -0700

    Bump esbuild and tailwind to match FreeBSD pkg

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :phx_example, PhxExample.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.25.0",
   phx_example: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -43,14 +43,14 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.3",
+  version: "4.1.11",
   phx_example: [
     args: ~w(
-      --config=tailwind.config.js
-      --input=css/app.css
-      --output=../priv/static/assets/app.css
+      --input=assets/css/app.css
+      --output=priv/static/assets/app.css
     ),
-    cd: Path.expand("../assets", __DIR__)
+    cd: Path.expand("..", __DIR__),
+    env: %{"NODE_PATH" => "/usr/local/lib/node_modules"}
   ]
 
 # Configures Elixir's Logger
